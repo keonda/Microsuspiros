0735b9ac74559338797697e9ff9812628e26d551
joaquine <techjoaquin@gmail.com>
2026-04-20 18:15:26 -0700

    Increase upload body size limit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
+    proxyClientMaxBodySize: "260mb",
     serverActions: {
       bodySizeLimit: "260mb"
     }
